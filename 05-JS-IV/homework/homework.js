dc23f8e4b5a739f0524d99ee947f12785d93ac1f
GeoLinkin <geoalfa86@gmail.com>
2021-10-16 11:26:49 -0500

    Solucion 05

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,14 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  const Gato = {  
+    nombre: nombre,
+    edad: edad,
+    meow: function (){
+      return("Meow!");
+    } 
+  }
+  return Gato;
 }
 
 
@@ -14,13 +22,19 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto[property] = null;
+  
+  return objeto;
+
 }
+
 
 function invocarMetodo(objeto, metodo) {
   // "metodo" es una cadena que contiene el nombre de un método (funcion) en el objeto
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  objeto.metodo();
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
