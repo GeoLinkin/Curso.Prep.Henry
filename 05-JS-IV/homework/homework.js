6a06d81cc6f4016ad01c8195926c2686e4461431
GeoLinkin <geoalfa86@gmail.com>
2021-10-16 20:42:51 -0500

    Solucion 05

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -86,7 +86,8 @@ function tienePropiedad(objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
-  objeto.hasOwnProperty(propiedad);
+  return objeto.hasOwnProperty(propiedad);
+  
 }
 
 function verificarPassword(usuario, password) {
@@ -94,12 +95,18 @@ function verificarPassword(usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
+  if(usuario["password"]===password){
+    return true;
+  }
+  return false;
 }
 
 function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password = nuevaPassword;
+  return usuario;
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
@@ -107,6 +114,8 @@ function agregarAmigo(usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // // Tu código:
+  usuario.amigos.push(nuevoAmigo);
+  return usuario;
 }
 
 function pasarUsuarioAPremium(usuarios) {
@@ -115,6 +124,10 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  for(i=0; i<usuarios.length; i++){
+    usuarios[i].esPremium = true;
+  }
+  return usuarios;
 }
 
 function sumarLikesDeUsuario(usuario) {
@@ -124,7 +137,24 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+
+  /* usuario:
+      posts: [post{0}
+              likes: 10]
+             [post{1}
+                likes: 100]
+              [post{2}
+                likes: 20]
+  */
+ var  acumulador = 0;
+ for(var i=0; i <usuario.posts.length; i++){
+   acumulador += usuario.posts[i].likes;
+ }
+ return acumulador;
 }
+
+
+
 
 function agregarMetodoCalculoDescuento(producto) {
   // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento"
@@ -136,6 +166,14 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+  var descuento= 0;
+  var precioDescuento= 0;
+  producto.calcularPrecioDescuento = function(){
+   
+    return this.precio - (this.precio * this.porcentajeDeDescuento);
+  }
+
+  return producto;
 
 }
 
